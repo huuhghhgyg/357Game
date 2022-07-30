812e6281bcd83d7ea2be83f63c986a6cef00a442
huuhghhgyg <huuhghhgyg@outlook.com>
2022-07-30 18:50:21 +0800

    添加生成对抗记录脚本

---
+++ 357Generator/main.py
@@ -0,0 +1,35 @@
+import random
+
+# 参数设置
+filePath = './output.csv'
+
+round = input("请输入回合数：")
+# 写入标题
+with open(filePath, 'a') as output_file:
+    output_file.write("B1,B2,B3,SelectBlock,Count,IsWinner\n")
+
+for i in range(1, int(round) + 1):
+    print("回合", i)
+    dashboard = [3, 5, 7]
+    roundList = []  # 先定义空集合
+    player = 1  # 玩家1操作(切换后)
+    # 回合循环
+    while dashboard[0] + dashboard[1] + dashboard[2] > 0:
+        player=1-player
+
+        selectBlock = random.randint(0, 2)  # 选择的方块(0,1,2)
+        if dashboard[selectBlock] == 0:
+            continue  # 如果选中的方块无可操作项，则跳到下一个循环
+        else:
+            # 选择数量
+            count = random.randint(1, dashboard[selectBlock])
+            roundList.append([dashboard[0],dashboard[1],dashboard[2],selectBlock,count,player])
+            dashboard[selectBlock] -= count
+
+    # 回合循环完毕，此时player为赢家
+    listLength = len(roundList) # 列表长度
+
+    with open(filePath, 'a') as output_file:
+        for i in range(0,listLength):
+            roundList[i][5] = 1 if roundList[i][5]==player else 0
+            output_file.write("%s,%s,%s,%s,%s,%s\n" % (roundList[i][0],roundList[i][1],roundList[i][2],roundList[i][3],roundList[i][4],roundList[i][5]))
