cf4d2fa49274fa547796276bf64e8782fc8692f9
huuhghhgyg <huuhghhgyg@outlook.com>
2022-07-31 15:34:51 +0800

    修正代码逻辑

--- 357Generator/main.py
+++ 357Generator/main.py
@@ -15,21 +15,31 @@ for i in range(1, int(round) + 1):
     player = 1  # 玩家1操作(切换后)
     # 回合循环
     while dashboard[0] + dashboard[1] + dashboard[2] > 0:
-        player=1-player
-
         selectBlock = random.randint(0, 2)  # 选择的方块(0,1,2)
         if dashboard[selectBlock] == 0:
             continue  # 如果选中的方块无可操作项，则跳到下一个循环
         else:
+            player = 1-player  # 切换玩家
+
             # 选择数量
             count = random.randint(1, dashboard[selectBlock])
-            roundList.append([dashboard[0],dashboard[1],dashboard[2],selectBlock,count,player])
+            roundList.append([dashboard[0], dashboard[1],
+                             dashboard[2], selectBlock, count, player])
             dashboard[selectBlock] -= count
 
-    # 回合循环完毕，此时player为赢家
-    listLength = len(roundList) # 列表长度
+    # 回合循环完毕，此时player为输家
+    listLength = len(roundList)  # 列表长度
+
+    # for debug:
+    # print("listLength:", listLength)
+    # print(roundList)
 
     with open(filePath, 'a') as output_file:
-        for i in range(0,listLength):
-            roundList[i][5] = 1 if roundList[i][5]==player else 0
-            output_file.write("%s,%s,%s,%s,%s,%s\n" % (roundList[i][0],roundList[i][1],roundList[i][2],roundList[i][3],roundList[i][4],roundList[i][5]))
+        for i in range(0, listLength):
+            roundList[i][5] = 0 if roundList[i][5] == player else 1 # 将玩家代号转为输赢代号
+
+            output_file.write("%s,%s,%s,%s,%s,%s\n" % (
+                roundList[i][0], roundList[i][1], roundList[i][2], roundList[i][3], roundList[i][4], roundList[i][5]))
+            
+            # for debug
+            # print(roundList[i])
